4693687114e9f91c920d72e8eb22598b5c91dc84
Cranky-dev <130955083+Cranky-dev@users.noreply.github.com>
2023-10-10 00:41:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function color(selectorType, selectorValue, styleValue) {
   if (selectorType == "tag" || selectorType == "tagName") {
-    const obj = document.getElementByTagName(selectorValue)
-
-}
+    const obj = document.getElementByTagName(selectorValue);
+    obj.style.color(styleValue);
+  }
   if (selectorType == "id" || selectorType == "tagId") {
-    
+    const obj = document.getElementById(selectorValue);
+    obj.style.color(styleValue);
   }
   if (selectorType == "class" || selectorType == "tagClass") {
-    
+
   }
 }
