86a463661e960e3f788208494a137256247c5e66
Cranky-dev <130955083+Cranky-dev@users.noreply.github.com>
2023-10-10 00:39:33 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function color(selectorType, selectorValue, styleValue) {
+  if (selectorType == "tag" || selectorType == "tagName") {
+    const obj = document.getElementByTagName(selectorValue)
+
+}
+  if (selectorType == "id" || selectorType == "tagId") {
+    
+  }
+  if (selectorType == "class" || selectorType == "tagClass") {
+    
+  }
+}
